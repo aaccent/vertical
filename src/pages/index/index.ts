883e672deee50a8b3509b0267206eb38920af79f
momo934 <ask4r.safiullin@yandex.ru>
2023-08-24 18:27:43 +0300

    quick filter

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,6 +1,7 @@
 import Swiper from 'swiper'
 import {Navigation, Autoplay} from "swiper/modules"
 import { initOfferSwiper } from 'global/components/pageBlocks/offerSwiper'
+import "components/ui/quickFilter"
 
 const leadSwiper = new Swiper('.lead-section__swiper', {
   navigation:{nextEl: ".page-viewer__right", prevEl: ".page-viewer__left"},
