e896d7904c83d1d15560f546d367584aa53465a3
momo934 <ask4r.safiullin@yandex.ru>
2023-08-30 21:31:01 +0300

    building status block

--- src/global/components/ui/pageViewer/index.ts
+++ src/global/components/ui/pageViewer/index.ts
@@ -1,3 +1,5 @@
+import { describeArc } from "global/features/arcProgress"
+
 const initPageViewer = (swiper: any, ) => {
   const pageViewer = document.querySelector('.page-viewer') as any
 
@@ -8,25 +10,7 @@ const initPageViewer = (swiper: any, ) => {
   let stepAngle = 360 / swiper.slides.length
   let currentAngle = 360 / swiper.slides.length
 
-  function polarToCartesian(centerX: number, centerY: number, radius: number, angleInDegrees: number) {
-    var angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0
 
-    return {
-      x: centerX + radius * Math.cos(angleInRadians),
-      y: centerY + radius * Math.sin(angleInRadians),
-    }
-  }
-
-  function describeArc(x: number, y: number, radius: number, startAngle: number, endAngle: number) {
-    var start = polarToCartesian(x, y, radius, endAngle - 0.0001)
-    var end = polarToCartesian(x, y, radius, startAngle)
-
-    var largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1'
-
-    var d = ['M', start.x, start.y, 'A', radius, radius, 0, largeArcFlag, 0, end.x, end.y].join(' ')
-
-    return d
-  }
 
   const renderArc = (angle: number) => {
     document.querySelector('#progress')?.setAttribute('d', describeArc(65, 65, 63, 360 - angle, 360))
