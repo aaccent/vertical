d38f9827f5750a1029a001ebb881c52f20efeb96
sevastyan <foxhable@gmail.com>
2023-10-16 17:48:29 +0300

    refactor: join scripts in one file

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,3 +1,11 @@
 import "layout/general/header"
 import "layout/general/footer"
 import "components/ui/dropdown"
+
+// Pages
+import 'pages/index'
+import 'pages/about'
+import 'pages/article'
+import 'pages/pressCenter'
+import 'pages/project'
+import 'pages/projectList'
