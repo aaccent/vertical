a4261d47435a603dff98ffa1cfe2b60aeb1ae7e9
sevastyan <foxhable@gmail.com>
2023-10-19 17:11:27 +0300

    refactor: extract slider function from hero-index slider

---
+++ src/global/features/slider/index.ts
@@ -0,0 +1,138 @@
+export interface Slide extends HTMLElement {
+  position: number
+}
+
+export interface Slider<TRawSlide extends HTMLElement = HTMLElement, TSlide extends Slide = Slide> {
+  options: SliderOptions<TRawSlide, TSlide>
+  container: HTMLElement | null
+  previousSlide: TSlide | null
+  currentSlide: TSlide | null
+  slideProgress: number
+  slides: TSlide[]
+  init: () => void
+  initSlide: (rawSlide: TRawSlide, index: number) => void
+  calcIndex: (newIndex: number) => number
+  setSlide: (index: number) => void
+  slideNext: () => void
+  slideBack: () => void
+  startAutoplay: () => void
+}
+
+export interface SliderEvents<TRawSlide extends HTMLElement = HTMLElement, TSlide extends Slide = Slide> {
+  beforeInitSlide?: (slide: TRawSlide) => void
+  afterInit?: (slider: Slider<TRawSlide, TSlide>) => void
+  onSlideChange?: (slider: Slider<TRawSlide, TSlide>) => void
+  afterSlideChange?: (slider: Slider<TRawSlide, TSlide>, relativeNextSlide: TSlide) => void
+}
+
+export interface SliderOptions<TRawSlide extends HTMLElement = HTMLElement, TSlide extends Slide = Slide> {
+  container: string | HTMLElement
+  autoplayTime?: number
+  handlers?: SliderEvents<TRawSlide, TSlide>
+  transitionTime?: number
+}
+
+export function createSlider<TRawSlide extends HTMLElement = HTMLElement, TSlide extends Slide = Slide>(options: SliderOptions<TRawSlide, TSlide>) {
+  const slider: Slider<TRawSlide, TSlide> = {
+    options: options,
+    container: null,
+    previousSlide: null,
+    currentSlide: null,
+    slides: [],
+    slideProgress: 0,
+
+    initSlide(rawSlide, index) {
+      options.handlers?.beforeInitSlide?.(rawSlide)
+
+      const slide: TSlide = rawSlide as unknown as TSlide
+      slide.position = index
+
+      this.slides.push(slide)
+    },
+
+    init() {
+      const slider = this
+      const _container = options.container instanceof HTMLElement
+        ? options.container
+        : document.querySelector<HTMLElement>(options.container)
+
+      if (!_container) {
+        const isSelector = typeof options.container === 'string'
+        const text = isSelector
+          ? `Element for slider by selector ${options.container} doesnt exists in DOM`
+          : 'Element for slider not exists in DOM. Element:\n'
+        return console.warn(text, !isSelector ? options.container : '')
+      }
+
+      this.container = _container
+
+      _container
+        .querySelectorAll<TRawSlide>('.slider-slide')
+        .forEach((rawSlide, i) => this.initSlide.call(slider, rawSlide, i))
+
+      options.handlers?.afterInit?.(slider)
+      this.setSlide(0)
+    },
+
+    calcIndex(newIndex: number) {
+      if (newIndex > this.slides.length - 1) return 0
+      if (newIndex < 0) return this.slides.length - 1
+      return newIndex
+    },
+
+    setSlide(index: number) {
+      if (!this.container) return
+
+      const slider = this
+
+      this.currentSlide = this.slides[index]
+
+      options.handlers?.onSlideChange?.(slider)
+
+      setTimeout(() => this.startAutoplay.call(slider), options.transitionTime || 0)
+
+      this.currentSlide.classList.add('active-slide')
+      this.container.querySelector('.previous-slide')?.classList.remove('previous-slide')
+      this.previousSlide?.classList.add('previous-slide')
+      this.previousSlide?.classList.remove('active-slide')
+
+      const relativeNextSlide = this.slides[this.calcIndex(this.currentSlide.position + 1)]
+      const relativePrevSlide = this.slides[this.calcIndex(this.currentSlide.position - 1)]
+
+      this.container.querySelector('.relative-next-slide')?.classList.remove('relative-next-slide')
+      this.container.querySelector('.relative-prev-slide')?.classList.remove('relative-prev-slide')
+
+      relativeNextSlide.classList.add('relative-next-slide')
+      relativePrevSlide.classList.add('relative-prev-slide')
+
+      this.previousSlide = this.slides[index]
+      options.handlers?.afterSlideChange?.(slider, relativeNextSlide)
+    },
+
+    slideNext() {
+      this.setSlide(this.calcIndex(this.currentSlide!.position + 1))
+    },
+
+    slideBack() {
+      this.setSlide(this.calcIndex(this.currentSlide!.position - 1))
+    },
+
+    startAutoplay() {
+      if (!this.options.autoplayTime) return
+
+      const STEP = 1 / this.options.autoplayTime * 3
+
+      const autoplayInterval = setInterval(() => {
+        if (this.slideProgress >= 1) {
+          clearInterval(autoplayInterval)
+          this.slideProgress = 0
+          this.slideNext()
+        }
+
+        this.slideProgress += STEP
+      }, 1)
+    },
+  }
+  slider.init()
+  return slider
+}
--- src/pages/index/sections/hero-index/index.ts
+++ src/pages/index/sections/hero-index/index.ts
@@ -1,6 +1,7 @@
-import { initTextAnimation, TextWithAnimation } from "features/animations/text";
+import { initTextAnimation, TextWithAnimation } from 'features/animations/text'
+import { createSlider, Slide } from 'features/slider'
 
-interface RawSlide extends HTMLElement {
+interface RawImgSlide extends HTMLElement {
   dataset: {
     subtitle?: string
     title?: string
@@ -11,10 +12,9 @@ interface RawSlide extends HTMLElement {
   }
 }
 
-interface Slide extends RawSlide {
+interface ImgSlide extends RawImgSlide, Slide {
   img: HTMLImageElement
-  position: number
-  dataset: RawSlide['dataset'] & {
+  dataset: RawImgSlide['dataset'] & {
     maxWidth: string
   }
   startSlideAnimation: () => void
@@ -26,7 +26,7 @@ const subtitleContainer = document.querySelector('.hero-index__text-subtitle')
 const titleContainer = document.querySelector<TextWithAnimation>('.hero-index__text-title')
 const link = document.querySelector<HTMLAnchorElement>('a.hero-index__link')
 
-function setSlideText(slide: RawSlide | Slide) {
+function setSlideText(slide: RawImgSlide | ImgSlide) {
   if (!subtitleContainer || !titleContainer || !link) return
 
   if (!slide.dataset.title) {
@@ -57,112 +57,19 @@ function setSlideText(slide: RawSlide | Slide) {
   }, 25)
 }
 
-function createSlider() {
-  const slidesEls = document.querySelectorAll<RawSlide>('.hero-index__img-wrapper')
-  let previousSlide: Slide
-  let activeSlide: Slide
-  const slides: Slide[] = []
-
-  const SCALE_ANIMATION = 1500
-  const MAX_WIDTH_ANIMATION = 700
-  const SCALE = '1.4'
-
-  const AUTOPLAY_TIME = 3000
-  const STEP = 1 / AUTOPLAY_TIME * 3
-  let slideProgress = 0
-  let autoplayInterval: number
-
-  function init() {
-    setSlideText(slidesEls[0])
-
-    slidesEls.forEach((slide, index) => {
-      const img = slide.querySelector('img') as HTMLImageElement
-
-      img.style.scale = SCALE
-      setTimeout(() => img.style.transition = `scale ${SCALE_ANIMATION}ms ease-in-out`, 5)
-      const maxWidth = `${slide.offsetWidth}px`
-      slide.dataset.maxWidth = maxWidth
-      slide.style.maxWidth = index < 2 ? maxWidth : '0px'
-
-      function startSlideAnimation() {
-        slide.style.maxWidth = '0'
-        setTimeout(() => (img.style.scale = SCALE), MAX_WIDTH_ANIMATION)
-      }
-
-      const _slide: Slide = slide as Slide
-      _slide.img = img
-      _slide.position = index
-      _slide.startSlideAnimation = startSlideAnimation
-
-      slides.push(_slide)
-    })
-
-    Array
-      .from(slidesEls)
-      .reverse()
-      .forEach((slide, i) => slide.style.zIndex = String(i))
-
-    setTimeout(() => {
-      document.querySelector<TextWithAnimation>('.hero-index__title')?.playAnimation()
-    }, 25)
-    previousSlide = slides[0]
-    activeSlide = slides[0]
-    setTimeout(() => {
-      slides[0].img.style.scale = '1'
-      startAutoplay()
-    }, MAX_WIDTH_ANIMATION)
+createSlider<RawImgSlide, ImgSlide>({
+  container: '.hero-index__images',
+  autoplayTime: 3000,
+  handlers: {
+    beforeInitSlide(rawSlide) {
+      (rawSlide as ImgSlide).dataset.maxWidth = `${rawSlide.offsetWidth}px`
+      rawSlide.style.maxWidth = `${rawSlide.offsetWidth}px`
+    },
+    afterInit(slider) {
+      setSlideText(slider.slides[0])
+    },
+    onSlideChange(slider) {
+      setSlideText(slider.currentSlide!)
+    }
   }
-
-  function checkNewIndex(newIndex: number) {
-    if (newIndex > slides.length - 1) return 0
-    if (newIndex < 0) return slides.length - 1
-    return newIndex
-  }
-
-  function setSlide(index: number) {
-    activeSlide = slides[index]
-    previousSlide.startSlideAnimation()
-    previousSlide = slides[index]
-    setSlideText(activeSlide)
-
-    setTimeout(() => {
-      activeSlide.img.style.scale = '1'
-      setTimeout(startAutoplay, SCALE_ANIMATION)
-    }, MAX_WIDTH_ANIMATION)
-
-    const nextSlide = slides[checkNewIndex(index + 1)]
-
-    nextSlide.style.maxWidth = nextSlide.dataset.maxWidth
-  }
-
-  function nextSlide() {
-    setSlide(checkNewIndex(activeSlide.position + 1))
-  }
-
-  function prevSlide() {
-    setSlide(checkNewIndex(activeSlide.position - 1))
-  }
-
-  function startAutoplay() {
-    autoplayInterval = setInterval(() => {
-      if (slideProgress >= 1) {
-        clearInterval(autoplayInterval)
-        slideProgress = 0
-        nextSlide()
-      }
-
-      slideProgress += STEP
-    }, 1)
-  }
-
-  return {
-    init,
-    setSlide,
-    slides,
-    nextSlide,
-    prevSlide,
-  }
-}
-
-const slider = createSlider()
-slider.init()
+})
