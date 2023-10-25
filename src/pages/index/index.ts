79ed77941c8eceaf7c48a7d69a5407959c00d359
sevastyan <foxhable@gmail.com>
2023-10-25 09:50:45 +0300

    fix: script error of index page on other pages

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -7,40 +7,44 @@ import 'components/pageBlocks/map'
 import './sections/hero-index'
 
 void function () {
-  if (window.matchMedia('(max-width: 1200px)').matches) return
+  const seoBlock = document.querySelector<HTMLElement>('.seo-block')
+
+  if (!seoBlock) return
+
+  const text = seoBlock.querySelector<HTMLElement>('.seo-block__text')
+  let seoBlockParagraph = seoBlock.querySelector<HTMLElement>('.seo-block__text p')
+
+  if (!text || !seoBlockParagraph) return
+
+  text.style.height = `${seoBlockParagraph.getBoundingClientRect().height}px`
+
+  window.addEventListener('resize', () => {
+    text.style.height = `${seoBlockParagraph!.getBoundingClientRect().height}px`
+    seoBlock.classList.remove('seo-block_open')
+  })
+
+  const calcSeoBlockTextHeight = (seoBlock: any) => {
+    let height = 0
+    seoBlock.querySelectorAll('p').forEach((text: HTMLElement) => {
+      height += text.getBoundingClientRect().height
+    })
+    return height
+  }
+
+
+  document.querySelector('.seo-block__expand')?.addEventListener('click', () => {
+    if (seoBlock.classList.contains('seo-block_open')) {
+      seoBlock.classList.remove('seo-block_open')
+      text.style.height = `${seoBlockParagraph!.getBoundingClientRect().height}px`
+      return
+    }
+    const height = calcSeoBlockTextHeight(seoBlock)
+
+    text.style.height = `${height}px`
+    seoBlock.classList.add('seo-block_open')
+  })
 }()
 
-const seoBlock = document.querySelector('.seo-block') as HTMLElement
-const text = seoBlock.querySelector('.seo-block__text') as HTMLElement
-let seoBlockParagraph = (seoBlock.querySelector('.seo-block__text p') as HTMLElement)
-
-text.style.height = `${seoBlockParagraph.getBoundingClientRect().height}px`
-
-window.addEventListener('resize', () => {
-  text.style.height = `${seoBlockParagraph.getBoundingClientRect().height}px`
-  seoBlock.classList.remove('seo-block_open')
-})
-
-const calcSeoBlockTextHeight = (seoBlock: any) => {
-  let height = 0
-  seoBlock.querySelectorAll('p').forEach((text: HTMLElement) => {
-    height += text.getBoundingClientRect().height
-  })
-  return height
-}
-
-
-document.querySelector('.seo-block__expand')?.addEventListener('click', () => {
-  if (seoBlock.classList.contains('seo-block_open')) {
-    seoBlock.classList.remove('seo-block_open')
-    text.style.height = `${seoBlockParagraph.getBoundingClientRect().height}px`
-    return
-  }
-  const height = calcSeoBlockTextHeight(seoBlock)
-
-  text.style.height = `${height}px`
-  seoBlock.classList.add('seo-block_open')
-})
 
 void function () {
   const animation = gsap.timeline()
@@ -48,10 +52,6 @@ void function () {
     .to('.our-projects__right', { translateY: 50 })
 
   new ScrollTrigger({
-    animation,
-    trigger: '.our-projects',
-    start: 'center center',
-    end: '+=500 center',
-    scrub: 2,
+    animation, trigger: '.our-projects', start: 'center center', end: '+=500 center', scrub: 2,
   })
 }
