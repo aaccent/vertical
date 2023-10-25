ec39b3739c7bbe4fb9402ab73f41f1f400e75e41
sevastyan <foxhable@gmail.com>
2023-10-25 10:17:21 +0300

    fix: script error of button-switch on other pages

--- src/global/components/ui/buttons/index.ts
+++ src/global/components/ui/buttons/index.ts
@@ -1,58 +1,66 @@
-const buttonSwitch = document.querySelector('.button-switch') as HTMLElement
-const buttonSwitchMark = document.querySelector('.button-switch__mark') as HTMLElement
-const buttonWidth = buttonSwitch.getBoundingClientRect().width
+void function () {
+  const buttonSwitch = document.querySelector<HTMLElement>('.button-switch')
+  const buttonSwitchMark = document.querySelector<HTMLElement>('.button-switch__mark')
 
-let isPressed = false
-let pos = 0
-let onButton = false
+  if (!buttonSwitch || !buttonSwitchMark) return
 
-buttonSwitch.addEventListener("mouseenter", () => {
-  onButton = true
-})
+  initButtonSwitch(buttonSwitch, buttonSwitchMark)
+}()
 
-buttonSwitch.addEventListener("mouseleave", () => {
-  onButton = false
-})
- 
-buttonSwitch.addEventListener('mousedown', (e) => {
-  isPressed = true
-  e.preventDefault()
-  console.log("fadsfsd");
-  
-})
+function initButtonSwitch(buttonSwitch: HTMLElement, buttonSwitchMark: HTMLElement) {
+  const buttonWidth = buttonSwitch.getBoundingClientRect().width
+  let isPressed = false
+  let pos = 0
+  let onButton = false
 
-buttonSwitch.addEventListener('mousemove', (e) => {  
-  if (!isPressed) return
-  pos = e.clientX - buttonSwitch.getBoundingClientRect().x
-  if (pos - 10 > 0 && pos + 30 < buttonWidth) {
-    buttonSwitchMark.style.left = `${pos}px`
-  }
-})
+  buttonSwitch.addEventListener('mouseenter', () => {
+    onButton = true
+  })
 
-window.addEventListener('mouseup', (e) => {
-  const percent = (pos / buttonWidth) * 100
-  if(!onButton) return
-  if ((pos < 0.01 || pos === 0) && isPressed) {
-    if (buttonSwitchMark.style.left === '') {
+  buttonSwitch.addEventListener('mouseleave', () => {
+    onButton = false
+  })
+
+  buttonSwitch.addEventListener('mousedown', (e) => {
+    isPressed = true
+    e.preventDefault()
+    console.log('fadsfsd')
+
+  })
+
+  buttonSwitch.addEventListener('mousemove', (e) => {
+    if (!isPressed) return
+    pos = e.clientX - buttonSwitch.getBoundingClientRect().x
+    if (pos - 10 > 0 && pos + 30 < buttonWidth) {
+      buttonSwitchMark.style.left = `${pos}px`
+    }
+  })
+
+  window.addEventListener('mouseup', (e) => {
+    const percent = (pos / buttonWidth) * 100
+    if (!onButton) return
+    if ((pos < 0.01 || pos === 0) && isPressed) {
+      if (buttonSwitchMark.style.left === '') {
+        buttonSwitchMark.style.left = `${buttonWidth - 30}px`
+        buttonSwitch.dispatchEvent(new Event('buttonChecked'))
+      } else {
+        buttonSwitchMark.style.left = ''
+        buttonSwitch.dispatchEvent(new Event('buttonUnchecked'))
+      }
+      pos = 0
+      isPressed = false
+
+      return
+    }
+    isPressed = false
+
+    if (percent > 50) {
       buttonSwitchMark.style.left = `${buttonWidth - 30}px`
       buttonSwitch.dispatchEvent(new Event('buttonChecked'))
     } else {
       buttonSwitchMark.style.left = ''
-      buttonSwitch.dispatchEvent(new Event('buttonUnchecked'))      
+      buttonSwitch.dispatchEvent(new Event('buttonUnchecked'))
     }
     pos = 0
-    isPressed = false
-
-    return
-  }
-  isPressed = false
-
-  if (percent > 50) {
-    buttonSwitchMark.style.left = `${buttonWidth - 30}px`
-    buttonSwitch.dispatchEvent(new Event('buttonChecked'))
-  } else {
-    buttonSwitchMark.style.left = ''
-    buttonSwitch.dispatchEvent(new Event('buttonUnchecked'))
-  }
-  pos = 0
-})
+  })
+}
