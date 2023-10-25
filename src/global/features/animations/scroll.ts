fc200c8d2fb3bbe9e8f2379258a42d1e15421a24
sevastyan <foxhable@gmail.com>
2023-10-25 20:34:24 +0300

    style: format scroll.ts

--- src/global/features/animations/scroll.ts
+++ src/global/features/animations/scroll.ts
@@ -1,12 +1,14 @@
 import LocomotiveScroll from 'locomotive-scroll'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import {ScrollTrigger} from 'gsap/ScrollTrigger'
 import gsap from 'gsap'
 
 gsap.registerPlugin(ScrollTrigger)
 
 const scroll = new LocomotiveScroll({
   multiplier: 0.6,
-  el: document.querySelector<HTMLElement>('[data-scroll-container]') || undefined, smooth: true, getDirection: true,
+  el: document.querySelector<HTMLElement>('[data-scroll-container]') || undefined,
+  smooth: true,
+  getDirection: true,
 })
 
 interface LocoScroll {
@@ -19,14 +21,23 @@ interface LocoScroll {
   }
 }
 
-ScrollTrigger.scrollerProxy('[data-scroll-container]', {
-  scrollTop(value) {
-    return arguments.length
-      ? void scroll.scrollTo(value!, { disableLerp: true })
-      : (scroll as unknown as LocoScroll).scroll.instance.scroll.y
-  }, getBoundingClientRect() {
-    return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight }
-  }, pinType: document.querySelector<HTMLElement>('[data-scroll-container]')?.style.transform ? 'transform' : 'fixed',
-})
+ScrollTrigger.scrollerProxy('[data-scroll-container]',
+  {
+    scrollTop(value) {
+      return arguments.length
+        ? void scroll.scrollTo(value!,
+          {disableLerp: true})
+        : (scroll as unknown as LocoScroll).scroll.instance.scroll.y
+    },
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      }
+    },
+    pinType: document.querySelector<HTMLElement>('[data-scroll-container]')?.style.transform ? 'transform' : 'fixed',
+  })
 
 scroll.on('scroll', ScrollTrigger.update)
