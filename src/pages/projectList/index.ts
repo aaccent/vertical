908a0929b6c368a6ba549f32fe6d965aa6a4f5ad
sevastyan <foxhable@gmail.com>
2023-10-26 15:59:45 +0300

    feat: animations to project-list

--- src/pages/projectList/index.ts
+++ src/pages/projectList/index.ts
@@ -1,2 +1,30 @@
 import 'components/ui/quickFilter'
 import 'components/pageBlocks/filter'
+import gsap from 'gsap'
+import {ScrollTrigger} from 'gsap/ScrollTrigger'
+
+void function () {
+  const projects = document.querySelector('.project__list')
+  if (!projects || matchMedia('(max-width: 1200px)').matches) return
+
+  const animation = gsap.timeline()
+    .fromTo('.project__list__row:nth-child(even) .project__list__item:last-child, .project__list__row:nth-child(odd) .project__list__item:first-child', {
+      width: '52%',
+    }, {
+      width: '100%',
+      height: 485,
+    }, 0)
+    .to('.project__list__row:nth-child(odd) .project__list__item:last-child, .project__list__row:nth-child(even) .project__list__item:first-child', {
+      width: '100%',
+      height: 485,
+    }, 0)
+
+  new ScrollTrigger({
+    scroller: '[data-scroll-container]',
+    animation,
+    trigger: projects,
+    start: 'top center',
+    end: 'bottom+=25% center',
+    scrub: 1,
+  })
+}()
