e57bbf27add155d0f77f520495ab9ee9665974e1
sevastyan <foxhable@gmail.com>
2023-10-27 19:45:54 +0300

    feat: animations to project-header

--- src/pages/project/index.ts
+++ src/pages/project/index.ts
@@ -1,3 +1,4 @@
+import gsap from 'gsap'
 import Swiper from 'swiper'
 import { Navigation, Autoplay } from 'swiper/modules'
 import 'components/pageBlocks/map'
@@ -5,20 +6,50 @@ import 'components/pageBlocks/filterPopup'
 import { initCustomSwiper } from 'features/slider/customSwiper'
 import { createSwiperPagination } from 'features/slider/pagination'
 
-const _gallerySwiper = new Swiper('.gallery__swiper', {
-  navigation: {
-    nextEl: '.page-viewer__right',
-    prevEl: '.page-viewer__left',
-  },
-  loop: true,
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  modules: [ Navigation, Autoplay ],
-})
+void function () {
+  const gallerySwiperEl = document.querySelector('.gallery__swiper')
+  if (!gallerySwiperEl) return
 
-createSwiperPagination(document.querySelector('.gallery .slider-pagination'), initCustomSwiper(_gallerySwiper))
+  createSwiperPagination(
+    document.querySelector('.gallery .slider-pagination'),
+    initCustomSwiper(
+      new Swiper('.gallery__swiper', {
+        navigation: {
+          nextEl: '.page-viewer__right',
+          prevEl: '.page-viewer__left',
+        },
+        loop: true,
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+        modules: [ Navigation, Autoplay ],
+      })
+    ),
+  )
+}()
+
+void function () {
+  const animation = gsap.timeline()
+    .from('.project-header__image img', {
+      duration: 5,
+      scale: 1.4,
+    }, 0)
+    .textAppearing('.project-header__title', {
+    }, 0)
+    .fadeUp('.project-header__actions', { yPercent: 120 }, '<0.4')
+    .fadeUp('.project-header__list__item', {}, '<0.4')
+    .from('.project-header__list', {
+      duration: 1.5,
+      '--before-width': '0%',
+    }, '<0.4')
+
+  gsap.utils.toArray('.project-header__list__item').forEach(item => {
+    animation.from(item as HTMLElement, {
+      '--after-height': '0%',
+    }, '<0.15')
+  })
+}()
 
 // initOfferSwiper('.offer__swiper')
 
