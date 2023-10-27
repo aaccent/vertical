2356d9b3a7915310b51f755c4305414edb6e863e
sevastyan <foxhable@gmail.com>
2023-10-27 13:51:38 +0300

    style: format pages/project/index.ts

--- src/pages/project/index.ts
+++ src/pages/project/index.ts
@@ -1,21 +1,21 @@
 import Swiper from 'swiper'
-import {Navigation, Autoplay} from 'swiper/modules'
+import { Navigation, Autoplay } from 'swiper/modules'
 import 'components/pageBlocks/map'
-import {initPageViewer} from 'global/components/ui/pageViewer'
-import {initOfferSwiper} from 'global/components/pageBlocks/offerSwiper'
-import {renderArc} from 'global/features/arcProgress'
 import 'components/pageBlocks/filterPopup'
-import {initCustomSwiper} from 'features/slider/customSwiper'
-import {createSwiperPagination} from 'features/slider/pagination'
+import { initCustomSwiper } from 'features/slider/customSwiper'
+import { createSwiperPagination } from 'features/slider/pagination'
 
 const _gallerySwiper = new Swiper('.gallery__swiper', {
-  navigation: {nextEl: '.page-viewer__right', prevEl: '.page-viewer__left'},
+  navigation: {
+    nextEl: '.page-viewer__right',
+    prevEl: '.page-viewer__left',
+  },
   loop: true,
   autoplay: {
     delay: 3000,
     disableOnInteraction: false,
   },
-  modules: [Navigation, Autoplay],
+  modules: [ Navigation, Autoplay ],
 })
 
 createSwiperPagination(document.querySelector('.gallery .slider-pagination'), initCustomSwiper(_gallerySwiper))
@@ -28,9 +28,12 @@ createSwiperPagination(document.querySelector('.gallery .slider-pagination'), in
 // renderArc(progressBar.querySelector('#progress') as HTMLElement, 360 * (progress / 100), 120)
 
 const galleryPopupDesktop = new Swiper('.gallery-popup__swiper', {
-  navigation: {nextEl: '.gallery-popup__right', prevEl: '.gallery-popup__left'},
+  navigation: {
+    nextEl: '.gallery-popup__right',
+    prevEl: '.gallery-popup__left',
+  },
 
-  modules: [Navigation, Autoplay],
+  modules: [ Navigation, Autoplay ],
 })
 
 const galleryPopupMobile = new Swiper('.gallery-popup__mobile-swiper', {})
