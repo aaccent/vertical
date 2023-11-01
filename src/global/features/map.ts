5dcb19419cb49adabf06455dee7057351bd826dd
sevastyan <foxhable@gmail.com>
2023-11-01 13:13:33 +0300

    fix: zooming and moving map

--- src/global/features/map.ts
+++ src/global/features/map.ts
@@ -5,31 +5,21 @@ void function () {
   const mapContainer = document.querySelector<HTMLElement>('.map__view')
   if (!mapContainer) return
 
-  new Map({
+  const map = new Map({
     container: mapContainer,
     style: 'mapbox://styles/seva-aaccent/clofebdm4004m01qs07pmdopg',
     center: [ 37.6170, 55.7554 ],
     zoom: 14,
     accessToken: 'pk.eyJ1Ijoic2V2YS1hYWNjZW50IiwiYSI6ImNsb2ZlNzR0NDByajUya3FwcmQ4bHdoZG8ifQ.2oZ5rpkSs2dKoP5a10lkcg',
+    dragRotate: false,
+    cooperativeGestures: true
   })
 
-  let isHoverMapContainer: boolean = false
-
-  mapContainer.addEventListener('mouseenter', (e) => {
-    isHoverMapContainer = true
-  })
-
-  mapContainer.addEventListener('mouseleave', (e) => {
-    isHoverMapContainer = false
-  })
-
-  window.addEventListener('keydown', (e) => {
-    if (e.key !== 'Control' || !isHoverMapContainer) return
-    scroll.stop()
-  })
-
-  window.addEventListener('keyup', (e) => {
-    if (e.key !== 'Control') return
+  mapContainer.addEventListener('wheel', (e) => {
     scroll.start()
-  })
+    if (!e.ctrlKey) return
+
+    scroll.stop()
+    e.preventDefault()
+  }, true)
 }()
